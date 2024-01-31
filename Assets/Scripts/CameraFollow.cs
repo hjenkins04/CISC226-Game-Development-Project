298a423d2153a590447189adb95e4e1e1b897a93
WillW0426 <149738570+WillW0426@users.noreply.github.com>
2024-01-30 19:23:50 -0500

    made testing scene with a character controller

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Just matching the camera pos with player pos
+        Transform playerPos = player.transform;
+
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y, -10);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.UI.Image;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Changeable variables
+    public float speed;
+    public float jumpForce;
+    public float downwardsRaycastDistance;
+
+    private Rigidbody2D rb;
+
+    //Visible private variable 
+    [SerializeField]
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Ged rigidbody component of the player game object
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input axis for horizontal movement.
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        //If on ground
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+            //jump when w is pressed
+            if (Input.GetKeyDown(KeyCode.W))
+            {   
+                //jump by adding an impulse force upwards
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+        //Maybe want less mobility in the air
+        else
+        {
+            rb.velocity = new Vector2(horizontalInput * (speed/2), rb.velocity.y);
+        }
+    }
+  
+    //check for when the player first touches an object
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //When the collision is on a platform
+        if(collision.gameObject.CompareTag("Platform"))
+        {
+            //downwards raycast to check if the platform is below the player, we don't want to say the player is grounded when its their head touching the bottom of a platform
+            RaycastHit2D rcHit = Physics2D.BoxCast(transform.position, new Vector2(1f, 1f), 0f, Vector2.down, downwardsRaycastDistance);
+            
+            //Check if there is a hit
+            if (rcHit.collider != null)
+            {
+                //player is on the ground
+                isGrounded = true;
+            }          
+        }
+    }
+
+    //Same thing as collision enter, but its exit.
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //If they exited a platform, the player is not on the ground anymore
+        if(collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = false;
+        }
+    }
+
+}

ea998bcb75112f739c6e1ed75890e95d23293e61
Ash <88748252+Ash-Taraghi@users.noreply.github.com>
2024-03-01 00:33:18 -0500

    added setting ui

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,10 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static AudioManager Instance;
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSource;
+
+    public void Awake(){
+        if (Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start(){
+        PlayMusic("MainMenuTheme");
+    }
+
+    public void PlayMusic(string name){
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s == null){
+            Debug.Log("Sound Not Found");
+        }
+        else{
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name){
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null){
+            Debug.Log("Sound Not Found");
+        }
+        else{
+            sfxSource.PlayOneShot(s.clip);
+            musicSource.Play();
+        }
+    }
+
 }

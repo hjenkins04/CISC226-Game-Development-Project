3a90ebb8b74478181477b988d200245e455d958b
Ash <88748252+Ash-Taraghi@users.noreply.github.com>
2024-02-29 23:14:51 -0500

    added button hover audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+}

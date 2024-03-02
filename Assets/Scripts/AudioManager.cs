ab3edc3d8ff7cc4b51d6a5bea69f7491901f5a7f
Ash <88748252+Ash-Taraghi@users.noreply.github.com>
2024-03-01 19:27:55 -0500

    working on sliders

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -48,4 +48,19 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    public void ToggleMusic(){
+        musicSource.mute = !musicSource.mute;
+    }
+
+    public void ToggleSFX(){
+        sfxSource.mute = !sfxSource.mute;
+    }
+    public void MusicVolume(float volume){
+        musicSource.volume = volume;
+    }
+
+    public void SFXVolume(float volume){
+        sfxSource.volume = volume;
+    }
+
 }

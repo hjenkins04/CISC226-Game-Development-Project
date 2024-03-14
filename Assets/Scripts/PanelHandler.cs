b028affdac9bf777ef6b1d484479bfca3552bdc2
Ash <88748252+Ash-Taraghi@users.noreply.github.com>
2024-03-14 01:14:05 -0400

    duplicate chests have pop ups. Bug: some buttons arn't sleectable on duplciate chest?

--- Assets/Scripts/PanelHandler.cs
+++ Assets/Scripts/PanelHandler.cs
@@ -4,35 +4,26 @@ using UnityEngine;
 
 public class PanelHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    
- public GameObject panel;
+    public GameObject panel;
     private bool isPanelVisible = false;
 
-   
+    // Reference to an instance of TextVisibility
+    public TextVisibilty textVisibiltyScript;
 
     void Update()
     {
-        // Check if the designated key is pressed
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //     // Toggle the visibility of the panel
-        //     if (isPanelVisible)
-        //     {
-        //         HidePanel();
-        //     }
-        //     else
-        //     {
-        //         ShowPanel();
-        //     }
-        // }
-
-        if (TextVisibilty.interactable == false){
-            HidePanel();
-        }
-        else{
-            ShowPanel();
+        // Check if the textVisibilityScript reference is not null
+        if (textVisibiltyScript != null)
+        {
+            // Access the interactable property directly
+            if (!textVisibiltyScript.interactable)
+            {
+                HidePanel();
+            }
+            else
+            {
+                ShowPanel();
+            }
         }
     }
 
